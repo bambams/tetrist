f434e32c3806356cf5b11588054b9902eebe649a
Brandon McCaig <bamccaig@gmail.com>
2017-09-26 22:40:47 -0400

    Draw hacked lines around piece I.

--- main.c
+++ main.c
@@ -258,6 +258,10 @@ int create_piece_i(ALLEGRO_BITMAP ** sprite) {
     al_draw_filled_rectangle(_0T, _0T,
                              _4T, _1T,
                              cyan);
+    al_draw_rectangle(_0T + 4/2, _0T + 4/2, _1T - 4/2, _1T - 4/2, black, 4);
+    al_draw_rectangle(_1T + 4/2, _0T + 4/2, _1T + _1T - 4/2, _1T - 4/2, black, 4);
+    al_draw_rectangle(_2T + 4/2, _0T + 4/2, _2T + _1T - 4/2, _1T - 4/2, black, 4);
+    al_draw_rectangle(_3T + 4/2, _0T + 4/2, _3T + _1T - 4/2, _1T - 4/2, black, 4);
 
     return 1;
 }
