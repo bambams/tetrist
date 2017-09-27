b8f3ac8791bc845c01fdf6b20700aba0ebbc10bd
Brandon McCaig <bamccaig@gmail.com>
2017-09-27 00:29:29 -0400

    Draw piece O using colored block.

--- main.c
+++ main.c
@@ -357,13 +357,19 @@ int create_piece_o(ALLEGRO_BITMAP ** sprite) {
         return 0;
     }
 
-    al_draw_filled_rectangle(_0T, _0T,
-                             _2T, _2T,
-                             yellow);
-    al_draw_rectangle(_0T + 4/2, _0T + 4/2, _1T - 4/2, _1T - 4/2, black, 4);
-    al_draw_rectangle(_1T + 4/2, _0T + 4/2, _1T + _1T - 4/2, _1T - 4/2, black, 4);
-    al_draw_rectangle(_0T + 4/2, _1T + 4/2, _1T - 4/2, _1T + _1T - 4/2, black, 4);
-    al_draw_rectangle(_1T + 4/2, _1T + 4/2, _1T + _1T - 4/2, _1T + _1T - 4/2, black, 4);
+    ALLEGRO_BITMAP * block = NULL;
+
+    if(!create_block(&block, yellow)) {
+        al_destroy_bitmap(*sprite);
+        *sprite = NULL;
+        return 0;
+    }
+
+    al_set_target_bitmap(*sprite);
+    draw_block(block, 0, 0);
+    draw_block(block, 1, 0);
+    draw_block(block, 0, 1);
+    draw_block(block, 1, 1);
 
     return 1;
 }
